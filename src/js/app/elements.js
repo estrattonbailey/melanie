aff005b3f0faa135cd48656315ec0e1587d1ae39
estrattonbailey <ericstrattonbailey@gmail.com>
2016-10-02 21:03:09 -0400

    A few tweaks

--- src/js/app/elements.js
+++ src/js/app/elements.js
@@ -1,15 +1,18 @@
 import h0 from 'h0'
 
 export const div = h0('div')
-export const button = h0('a')({class: 'h1 mv05 mr1 inline-block'})
-export const title = h0('h1')
+export const button = h0('button')({class: 'h2 mv0 inline-block'})
+export const title = h0('h1')({class: 'mt0'})
 
 export const template = ({prompt, answers}, cb) => {
-  return div(
+  return div({class: 'question'})(
     title(prompt),
     div(
-      ...answers.map(a => button({
-        onclick: (e) => cb(a.next)
+      ...answers.map((a, i) => button({
+        onclick: (e) => cb(a.next),
+        style: {
+          color: window.__app.colors[i]
+        }
       })(a.value))
     )
   )
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,13 @@
 import app from './app'
 
+window.__app = {
+  colors: [
+    '#35D3E8',
+    '#FF4E42',
+    '#FFEA51'
+  ]
+}
+
 window.addEventListener('DOMContentLoaded', () => {
   app()
 })
