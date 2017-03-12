0eccab4f20f05aee1de0a256534a503c0faf7937
yomelaniejo <melaniejslattery@gmail.com>
2017-03-12 14:48:06 -0400

    Edit data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 module.exports = {
   pages: {
     work: {
-      title: `Look on my works, ye Mighty, and despair!`
+      title: `Work, work, work, work, work.`
     },
     about: {
       title: 'A Goddamn Lady',
@@ -11,42 +11,200 @@ module.exports = {
   },
   projects: [
     {
-      title: `Team USA Awards`,
-      caption: `Caption goes here`,
-      url: `work/team-usa-awards`,
+      title: `DEWggro Crag Powered by The Splat`,
+      caption: `A retro-inspired, soda-filled festival.`,
+      url: `work/dewggro-crag-powered-by-the-splat`,
       content: {
-        subtitle: 'Subtitle',
-        subsubtitle: 'Subsubtitle'
+        subtitle: `A retro-inspired, soda-filled festival.`,
+        subsubtitle: `
+Can you imagine climbing the iconic Aggro Crag from the 90’s Nickelodeon show, GUTS? Mountain Dew snagged the idea to raise awareness and cast votes for their DEWcision 2016 campaign, and the DEWggro Crag Powered by The Splat was born. Of course, such a marvel couldn’t merely live on its own, and an entire festival surrounded the setup, complete with dueling performers, soda sampling, giveaways, and more.
+`,
+        for: `The Michael Alan Group, in partnership with Viacom Special Events & Blue Revolver`
       },
       images: [
-        `http://unsplash.it/300/200`,
-        `http://unsplash.it/300/200`
+        `DEWggro-Crag-Nickelodeon-9.jpg`,
+        `DEWggro-Crag-Nickelodeon-8.jpg`,
+        `DEWggro-Crag-Nickelodeon-7.jpg`,
+        `DEWggro-Crag-Nickelodeon-6.jpg`,
+        `DEWggro-Crag-Nickelodeon-5.jpg`,
+        `DEWggro-Crag-Nickelodeon-4.jpg`,
+        `DEWggro-Crag-Nickelodeon-3.jpg`,
+        `DEWggro-Crag-Nickelodeon-2.jpg`,
+        `DEWggro-Crag-Nickelodeon-1.jpg`
       ]
     },
     {
-      title: `Grandfathered`,
-      caption: `Caption goes here`,
-      url: `work/grandfathered`,
+      title: `Grandfathered Pop-Up Barbershop`,
+      caption: `Bringing the GILF look back to LA.`,
+      url: `work/grandfathered-pop-up-barbershop`,
       content: {
-        subtitle: 'Subtitle',
-        subsubtitle: 'Subsubtitle'
+        subtitle: `Bringing the GILF look back to LA.`,
+        subsubtitle: `
+John Stamos (swoon) plays quite the grandpa in the FOX series, Grandfathered. To drive tune-in to this GILF, a pop-up barbershop was produced at The Grove in Los Angeles. With a custom design that felt fresh and modern and far-from-aged, we encouraged everyone to hop in to receive their own Grandfathered look.
+`,
+        for: `The Michael Alan Group`
       },
       images: [
-        `http://unsplash.it/300/200`,
-        `http://unsplash.it/300/200`
+        `Fox-Grandfathered-GILF-2.jpg`,
+        `Fox-Grandfathered-GILF-10.jpg`,
+        `Fox-Grandfathered-GILF-1.jpg`,
+        `Fox-Grandfathered-GILF-3.jpg`,
+        `Fox-Grandfathered-GILF-4.jpg`,
+        `Fox-Grandfathered-GILF-5.jpg`,
+        `Fox-Grandfathered-GILF-6.jpg`,
+        `Fox-Grandfathered-GILF-7.jpg`,
+        `Fox-Grandfathered-GILF-8.jpg`,
+        `Fox-Grandfathered-GILF-9.jpg`
+      ]
+    },
+    {
+      title: `USOC 2016 Team USA Awards`,
+      caption: `Yes, the Final Five were there.`,
+      url: `work/usoc-2016-team-usa-awards`,
+      content: {
+        subtitle: `Yes, the Final Five were there.`,
+        subsubtitle: `
+After an amazing year at the Rio Olympics, Team USA deserved a big celebration, and that’s precisely what the United States Olympic Committee made happen. The events of the evening included a red carpet procession, an Olympic ring ceremony, an awards show (later aired on NBC Sports Network), and a chic after party. Layout, stage design, branding, and decor were major pieces I helped tackle for this project.
+`,
+        for: `The Michael Alan Group`
+      },
+      images: [
+        `USOC-2016-Team-USA-Awards-13.jpg`,
+        `USOC-2016-Team-USA-Awards-4.jpg`,
+        `USOC-2016-Team-USA-Awards-1.jpg`,
+        `USOC-2016-Team-USA-Awards-2.jpg`,
+        `USOC-2016-Team-USA-Awards-3.jpg`,
+        `USOC-2016-Team-USA-Awards-5.jpg`,
+        `USOC-2016-Team-USA-Awards-6.jpg`,
+        `USOC-2016-Team-USA-Awards-7.jpg`,
+        `USOC-2016-Team-USA-Awards-8.jpg`,
+        `USOC-2016-Team-USA-Awards-9.jpg`,
+        `USOC-2016-Team-USA-Awards-10.jpg`,
+        `USOC-2016-Team-USA-Awards-11.jpg`,
+        `USOC-2016-Team-USA-Awards-12.jpg`
+      ]
+    },
+    {
+      title: `New Balance Press Event`,
+      caption: `Breaking News: Sweet kicks.`,
+      url: `work/new-balance-press-event`,
+      content: {
+        subtitle: `Breaking News: Sweet kicks.`,
+        subsubtitle: `
+When New Balance was ready to launch a new line of stylishly casual kicks, we ensured the press event matched the product’s allure. Instead of an overtly flashy soiree, we set the mood with a chic gallery event, complete with a floating shoe installation, live performance art, specialty cocktails, and tunes from DJ Brendan Fallis.
+`,
+        for: `The Michael Alan Group`
+      },
+      images: [
+        `New-Balance-1.jpg`,
+        `New-Balance-2.jpg`,
+        `New-Balance-3.jpg`,
+        `New-Balance-4.jpg`,
+        `New-Balance-5.jpg`,
+        `New-Balance-6.jpg`
+      ]
+    },
+    {
+      title: `Almay Simply American Bus Tour`,
+      caption: `Taking beauty on a road trip.`,
+      url: `work/almay-simply-american-bus-tour`,
+      content: {
+        subtitle: `Taking beauty on a road trip.`,
+        subsubtitle: `
+Almay stopped in 11 major cities while touring The American Look on their custom RV, offering free mini makeovers to attendees at State Fairs and Festivals across the country. Beyond the beauty, consumers were also invited to win Almay products and swag, or saunter over to a kissing booth to smooch their sweetheart. The activation included a partnership with the USO to raise money for our military and veterans.
+`,
+        for: `The Michael Alan Group`
+      },
+      images: [
+        `Almay-Experience-1.jpg`,
+        `Almay-Experience-2.jpg`,
+        `Almay-Experience-3.jpg`,
+        `Almay-Experience-4.jpg`,
+        `Almay-Experience-5.jpg`,
+        `Almay-Experience-6.jpg`,
+        `Almay-Experience-7.jpg`,
+        `Almay-Experience-8.jpg`,
+        `Almay-Experience-9.jpg`,
+        `Almay-Experience-10.jpg`,
+        `Almay-Experience-11.jpg`,
+        `Almay-Experience-12.jpg`,
+        `Almay-Experience-13.jpg`
+      ]
+    },
+    {
+      title: `The Michael Alan Group Blog`,
+      caption: `Built from the ground up.`,
+      url: `work/michael-alan-group-blog`,
+      content: {
+        subtitle: `Built from the ground up.`,
+        subsubtitle: `
+Seeking to improve SEO performance and social influence, The Michael Alan Group set out to build a blog. I managed these efforts from the beginning; crafting a content strategy, working with freelance writers, composing my own pieces, researching topics, sourcing visual material, and building each article online. While the initial going was slow and steady, we have built the beast into an efficient machine and continue to make progress toward the initial goals.
+`,
+        for: `The Michael Alan Group`
+      },
+      images: [
+        `image.jpg`,
+        `image2.jpg`
       ]
     }
   ],
   favs: [
     {
-      title: `How to be a Goddamn Lady`,
-      caption: `Caption goes here`,
-      url: `work/how-to-be-a-lady`
+      title: `The Floor is Lava`,
+      caption: `If the childhood game existed in real life.`,
+      url: `work/floor-is-lava`,
+      content: {
+        subtitle: `If the childhood game existed in real life.`,
+        subsubtitle: `
+Allow me to set the scene: You’re a young professional climbing the corporate ladder. You’ve been working long hours in a steel labyrinth somewhere, and you’re finally clocking out and heading home. Drowsily navigating your way to the exit, you took a wrong turn. You’ve come to a long hallway, covered by a bizarre smattering of platforms. The fluorescent buzz fills your head as you catch the caution sign. “ATTENTION: FLOOR IS LAVA.”
+
+Good luck, my friend.
+`
+      },
+      images: [
+        `Floor-Is-Lava-1.jpg`,
+        `Floor-Is-Lava-2.jpg`,
+        `Floor-Is-Lava-3.jpg`,
+        `Floor-Is-Lava-4.jpg`,
+        `Floor-Is-Lava-5.jpg`
+      ]
+    },
+    {
+      title: `How to Be a Goddamn Lady`,
+      caption: `Offering the best advice on the matter.`,
+      url: `work/how-to-be-a-lady`,
+      content: {
+        subtitle: `Offering the best advice on the matter.`,
+        subsubtitle: `
+Sure, the assignment was simply to build a site using parallax, but within it I discovered a grand opportunity. A chance to instruct women of the world on how to not merely be a lady, but be a goddamn lady. I assure you I am most qualified, for I have never burped or tooted or looked a gentleman in the eye, and I always take my tea with a shot of brandy.
+
+Jokey jokes. It’s all fun and games. See the whole thing here.
+`
+      },
+      images: [
+        `A-Goddamn-Lady-1.jpg`,
+        `A-Goddamn-Lady-2.png`,
+        `A-Goddamn-Lady-3.png`,
+        `A-Goddamn-Lady-4.png`,
+        `A-Goddamn-Lady-5.jpg`,
+      ]
     },
     {
       title: `Horse Piss`,
-      caption: `Caption goes here`,
-      url: `work/horse-piss`
+      caption: `Sounds appetizing, right?`,
+      url: `work/horse-piss`,
+      content: {
+        subtitle: `Sounds appetizing, right?`,
+        subsubtitle: `
+The beverage label: a classic project doled out to most design students. When tasked with this in my college days, I opted for a far-too-appealing craft beer brand. While the design may hit the target aesthetic, the ingredient list is vague and hopefully disconcerting. To carry the theme to it’s full potential, I completed the ensemble with a wooden crate full of straw. I mean, where else would you literally find Horse Piss?
+`
+      },
+      images: [
+        `Horse-Piss-2.jpg`,
+        `Horse-Piss-3.jpg`,
+        `Horse-Piss-1.jpg`,
+        `Horse-Piss-4.jpg`
+      ]
     }
   ]
 }
