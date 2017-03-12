2d4ae28c22994e5d2d2c0d3e070defec8e0207f6
yomelaniejo <melaniejslattery@gmail.com>
2017-03-12 16:24:21 -0400

    UPdate content

--- src/js/app/data/index.js
+++ src/js/app/data/index.js
@@ -1,22 +1,22 @@
 export default [
   {
     id: 1,
-    prompt: `hi! what brings you to this neck of the interwebs?`,
+    prompt: `Hi! What brings you to this neck of the web?`,
     answers: [
       {
-        value: 'who r u',
-        next: 2 
+        value: 'Who r u?',
+        next: 2
       },
       {
-        value: 'hiring',
+        value: `I'm hiring.`,
         next: 3
       },
       {
-        value: `it's your mom`,
+        value: `It's your mother.`,
         next: 4
       },
       {
-        value: `funny jokes`,
+        value: `Funny jokes, plz.`,
         next: 5
       }
     ]
@@ -24,14 +24,14 @@ export default [
 
   {
     id: 2,
-    prompt: `i'm melanie – a graphic designer working in experiential marketing & proud iowan trying to eat ALL the BLTs`,
+    prompt: `I'm melanie – a graphic designer working in experiential marketing & proud Iowan trying to eat ALL the BLTs.`,
     answers: [
       {
-        value: `what's experiential?`,
-        next: 6 
+        value: `What's experiential?`,
+        next: 6
       },
       {
-        value: `what's a BLT?`,
+        value: `What's a BLT?`,
         next: 7
       },
     ]
@@ -39,18 +39,18 @@ export default [
 
   {
     id: 3,
-    prompt: `rad! can i show you some projects i've worked on?`,
+    prompt: `Rad. Can I show you some projects I've worked on?`,
     answers: [
       {
-        value: `yes, please!`,
+        value: `Yes, please!`,
         next: '/work'
       },
       {
-        value: `nah, tell me about you`,
+        value: `Nah, tell me about you.`,
         next: '/about'
       },
       {
-        value: `i'll email you instead`,
+        value: `I'll email you instead.`,
         next: '/contact'
       },
     ]
@@ -58,12 +58,8 @@ export default [
 
   {
     id: 4,
-    prompt: `hi mom! i love you!`,
+    prompt: `Hi mom. I love you!`,
     answers: [
-      {
-        value: `:) i love you too!`,
-        next: 8
-      },
       {
         value: `jk, not your mom`,
         next: 9
@@ -73,14 +69,14 @@ export default [
 
   {
     id: 5,
-    prompt: `what's funnier than a rhetorical question?`,
+    prompt: `What's funnier than a rhetorical question?`,
     answers: [
       {
-        value: `yes`,
+        value: `Yes`,
         next: 10
       },
       {
-        value: `no`,
+        value: `No`,
         next: 'https://media.giphy.com/media/P2Hy88rAjQdsQ/giphy.gif'
       },
     ]
@@ -88,14 +84,14 @@ export default [
 
   {
     id: 6,
-    prompt: `experiential is this cool niche type of marketing, ya know?`,
+    prompt: `Experiential marketing engages directly with consumers, inviting them to particpate in the evolution of a brand.`,
     answers: [
       {
-        value: `sounds cool. what have you done?`,
+        value: `Sounds cool. What have you done?`,
         next: '/work'
       },
       {
-        value: `why do you like it?`,
+        value: `Why do you like it?`,
         next: 11
       },
     ]
@@ -103,48 +99,33 @@ export default [
 
   {
     id: 7,
-    prompt: `take a wild guess...`,
+    prompt: `You tell me.`,
     answers: [
       {
-        value: `beef liver toast`,
+        value: `Beef Liver Toast`,
         next: 'https://media.giphy.com/media/oFOs10SJSnzos/giphy.gif'
       },
       {
-        value: `blueberry lemon tart`,
+        value: `Berry Lemon Tart`,
         next: 'https://media.giphy.com/media/3o7TKwmnDgQb5jemjK/giphy.gif'
       },
       {
-        value: `bacon lettuce tomato`,
+        value: `Bacon Lettuce Tomato`,
         next: 'https://media.giphy.com/media/fqzxcmlY7opOg/giphy.gif'
       },
     ]
   },
 
   {
-    id: 8,
-    prompt: `so... can i ship laundry home to iowa?`,
-    answers: [
-      {
-        value: `of course!`,
-        next: 'https://media.giphy.com/media/11sBLVxNs7v6WA/giphy.gif'
-      },
-      {
-        value: `yeah, still not your mom...`,
-        next: 12
-      },
-    ]
-  },
-
-  {
     id: 9,
-    prompt: `clicking for fun, huh? good luck with this one.`,
+    prompt: `Clicking for fun? Good luck with this one.`,
     answers: [
       {
-        value: `blue pill`,
+        value: `Blue Pill`,
         next: 'https://media.giphy.com/media/G7GNoaUSH7sMU/giphy.gif'
       },
       {
-        value: `red pill`,
+        value: `Red Pill`,
         next: 'https://media.giphy.com/media/UjujGY3mA3Jle/giphy.gif'
       },
     ]
@@ -152,10 +133,10 @@ export default [
 
   {
     id: 10,
-    prompt: `pancakes or waffles?`,
+    prompt: `Pancakes or waffles?`,
     answers: [
       {
-        value: `french toast`,
+        value: `French Toast`,
         next: 'https://media.giphy.com/media/14nb6TlIRlaN1u/giphy.gif'
       },
     ]
@@ -163,48 +144,22 @@ export default [
 
   {
     id: 11,
-    prompt: `i like experiential because it's just super cool, okay?`,
+    prompt: `I like experiential because it's just super cool, okay?`,
     answers: [
       {
-        value: `what are your favorite projects?`,
+        value: `What are your favorite projects?`,
         next: 14
       },
       {
-        value: `i have questions! can i email you?`,
+        value: `I have questions! Can I email you?`,
         next: '/contact'
-      },
-    ]
-  },  
-
-  {
-    id: 12,
-    prompt: `taking this a little far don't you think?`,
-    answers: [
-      {
-        value: `sure am`,
-        next: 'https://media.giphy.com/media/qINsfDGI0z9yU/giphy.gif'
-      },
-      {
-        value: `must click ALL buttons`,
-        next: 13
-      },
-    ]
-  },
-
-  {
-    id: 13,
-    prompt: `yeah?`,
-    answers: [
-      {
-        value: `clicking this may harm a kitten`,
-        next: 'https://media.giphy.com/media/IgghkXWkdnEEo/giphy.gif'
       },
     ]
   },
 
   {
     id: 14,
-    prompt: `of course I love my own work, but these projects deserve some serious props`,
+    prompt: `I love the work I've done, but these projects deserve some serious props.`,
     answers: [
       {
         value: `project 1`,
