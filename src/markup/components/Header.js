1100ffbf4e289401863dc80a0db129505dc94eaa
yomelaniejo <melaniejslattery@gmail.com>
2020-06-16 13:22:05 -0500

    Reorder nav

--- src/markup/components/Header.js
+++ src/markup/components/Header.js
@@ -6,8 +6,8 @@ export default props => (
       <a className="nav__logo relative z1" href="/">melanie slattery</a>
       <nav id="menu" className="nav__inner inline-block flex-auto fixed fit-x px1 z0">
         <a className="block px1" href="/about/"><span>/about</span></a>
-        <a className="block px1" href="/experiential/"><span>/experiential</span></a>
         <a className="block px1" href="/design/"><span>/design</span></a>
+        <a className="block px1" href="/experiential/"><span>/experiential</span></a>
         <a className="block px1" href="/contact/"><span>/contact</span></a>
       </nav>
       <button id="toggle" className="hamburger block z10 relative">
