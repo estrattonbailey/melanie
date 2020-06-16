07472afe8f7a4504accc7709d412a121ef9cfa45
yomelaniejo <melaniejslattery@gmail.com>
2020-06-16 13:27:10 -0500

    Fix error

--- src/markup/components/Foot.js
+++ src/markup/components/Foot.js
@@ -6,7 +6,7 @@ export default props => (
       <hr className="cb"/>
       <nav className="nav__inner inline-block flex-auto">
         <a href="/about/"><span>/about</span></a>
-        <a href="/design/"><span>/design</span></a
+        <a href="/design/"><span>/design</span></a>
         <a href="/experiential/"><span>/experiential</span></a>
         <a href="/contact/"><span>/contact</span></a>
       </nav>
